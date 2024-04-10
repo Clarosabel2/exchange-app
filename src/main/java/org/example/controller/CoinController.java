ee4456cf19a242bec2143f57b182b88788d25117
Christian Claros <95495732+Clarosabel2@users.noreply.github.com>
2024-04-10 10:58:06 -0300

    Exceptions

--- src/main/java/org/example/controller/CoinController.java
+++ src/main/java/org/example/controller/CoinController.java
@@ -19,28 +19,44 @@ public class CoinController {
     public static int valueCoin;
 
     public static void searchCoin(int op) throws IOException, InterruptedException {
-        Gson gson = new Gson();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://v6.exchangerate-api.com/v6/bf1c002c4f4b62f4cab986c6/latest/" + coins[op-1]))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        if (statusOp(op)) {
+            Gson gson = new Gson();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://v6.exchangerate-api.com/v6/bf1c002c4f4b62f4cab986c6/latest/" + coins[op - 1]))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        CoinExchangeRate coinER = gson.fromJson(response.body(), CoinExchangeRate.class);
-        _coin = new Coin(coinER);
+            CoinExchangeRate coinER = gson.fromJson(response.body(), CoinExchangeRate.class);
+            _coin = new Coin(coinER);
+        }
     }
 
     public static double convertTo(double amount) {
-        double result = amount * _coin.getPrice().get(coins[valueCoin-1]);
+        double result = amount * _coin.getPrice().get(coins[valueCoin - 1]);
         return result;
     }
 
     public static String[] getCoinSlt() {
         String[] info = new String[3];
         info[0] = _coin.getSymbol();
-        info[1] = coins[valueCoin-1];
+        info[1] = coins[valueCoin - 1];
         info[2] = String.valueOf(_coin.getPrice().get(info[1]));
         return info;
+    }
+
+    public static boolean statusOp(int op) {
+        boolean flag = false;
+        for (int i = 1; i < coins.length + 1; i++) {
+            if (op == i) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            throw new NumberFormatException("Opcion Invalida");
+        }
+        return true;
     }
 }
--- src/main/java/org/example/view/Main.java
+++ src/main/java/org/example/view/Main.java
@@ -8,51 +8,70 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        int op = -1;
+        int op = -1, a = 0;
         while (true) {
-            System.out.println("""
-                    ******************************
-                             Bienvenido
-                    Elija la Divisa:
-                     1.[ARS] Peso Argentino
-                     2.[USD] Dolar Estadounidense
-                     3.[EUR] Euro
-                     4.[CLP] Peso Chileno
-                     5.[BOB] Peso Boliviano
-                     6.[PEN] Peso Peruano
-                     7.[BRL] Real Brasileño
-                     ******************************
-                    """);
-            CoinController.searchCoin(sc.nextInt());
-            System.out.println("""
-                    Elija a que Divisa desea convertir:
-                     1.[ARS] Peso Argentino
-                     2.[USD] Dolar Estadounidense
-                     3.[EUR] Euro
-                     4.[CLP] Peso Chileno
-                     5.[BOB] Peso Boliviano
-                     6.[PEN] Peso Peruano
-                     7.[BRL] Real Brasileño
-                     ******************************
-                    """);
-            CoinController.valueCoin = sc.nextInt();
+            try {
+                System.out.println("""
+                        *****************************************
+                                     Bienvenido                 *
+                        --------------------------------------  *
+                        Ingrese el numero de la opcion elegida. *
+                        Elija la Divisa:                        *
+                         1.[ARS] Peso Argentino                 *
+                         2.[USD] Dolar Estadounidense           *
+                         3.[EUR] Euro                           *
+                         4.[CLP] Peso Chileno                   *
+                         5.[BOB] Peso Boliviano                 *
+                         6.[PEN] Peso Peruano                   *
+                         7.[BRL] Real Brasileño                 *
+                         ****************************************
+                        """);
+                CoinController.searchCoin(sc.nextInt());
+
+                System.out.println("""
+                        Elija a que Divisa desea convertir:     *
+                         1.[ARS] Peso Argentino                 *
+                         2.[USD] Dolar Estadounidense           *
+                         3.[EUR] Euro                           *
+                         4.[CLP] Peso Chileno                   *
+                         5.[BOB] Peso Boliviano                 *
+                         6.[PEN] Sol Peruano                    *
+                         7.[BRL] Real Brasileño                 *
+                        *****************************************
+                        """);
+                a = sc.nextInt();
+                if (CoinController.statusOp(a)) {
+                    CoinController.valueCoin = a;
+                }
+
+                System.out.println("1 " + CoinController.getCoinSlt()[0] + " => $" + CoinController.getCoinSlt()[2] + " " + CoinController.getCoinSlt()[1]);
+                System.out.println("Ingrese el monto a convertir: ");
+                System.out.println("Son: " + CoinController.convertTo(sc.nextDouble()) + " " + CoinController.getCoinSlt()[1]);
+                do {
+                    System.out.println("""
+                            Desea convertir otra Divisia?
+                                1.Si
+                                2.No
+                            """);
+                    op = sc.nextInt();
+                    if (op != 1 && op != 2) {
+                        System.out.println("Opcion Incorrecta, intenteto nuevamente...");
+                    }
+
+                } while (op != 1 && op != 2);
+
+                if (op == 2) {
+                    op = 0;
+                }
+                if (op == 0) {
+                    System.out.println("Muchas gracias por usar nuestro servicio, hasta luego :D.");
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage() + ", intentelo nuevamente...");
+            }
 
 
-            System.out.println("1 "+CoinController.getCoinSlt()[0]+" => $"+CoinController.getCoinSlt()[2]+" "+CoinController.getCoinSlt()[1]);
-            System.out.println("Ingrese el monto a convertir: ");
-            System.out.println("Son: "+CoinController.convertTo(sc.nextDouble())+" "+CoinController.getCoinSlt()[1]);
-            System.out.println("""
-                    Desea convertir otra Divisia?
-                        1.Si
-                        2.No
-                    """);
-            if (sc.next().contains("2")) {
-                op = 0;
-            }
-            if (op == 0) {
-                System.out.println("Muchas gracias por usar nuestro servicio, hasta luego :D.");
-                break;
-            }
         }
     }
 }
